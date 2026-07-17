5e83391b38e0e4bcbd18a44c4cfb6e15a5f67618
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-17 23:24:57 +0200

    Voeg bedrijfsinstellingen voor documenten toe

---
+++ migrations/2026-07-17-company-document-settings.sql
@@ -0,0 +1,17 @@
+USE `sql7833304`;
+
+INSERT IGNORE INTO `settings` (`setting_key`,`setting_value`) VALUES
+('company_name','KlusHulp Noord'),
+('company_street',''),
+('company_house_number',''),
+('company_postal_code',''),
+('company_city',''),
+('company_country','Nederland'),
+('company_phone',''),
+('company_email',''),
+('company_website','https://www.klushulpnoord.nl'),
+('company_kvk',''),
+('company_vat_number',''),
+('company_iban',''),
+('document_terms','Op al onze offertes en facturen zijn onze algemene voorwaarden van toepassing.'),
+('slogan','Betaalbaar • Betrouwbaar • Voor Iedereen');
